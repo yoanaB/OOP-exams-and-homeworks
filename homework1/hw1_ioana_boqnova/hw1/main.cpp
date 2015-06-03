eb41d2cf4b39855558263ad42cdde5eb3bab06e5
yoanaB <yoana.boyanova95@gmail.bg>
2015-06-03 20:15:58 +0300

    Update main.cpp
    Добавени са извиквания на всички оператори.

--- homework1/hw1_ioana_boqnova/hw1/main.cpp
+++ homework1/hw1_ioana_boqnova/hw1/main.cpp
@@ -1,5 +1,7 @@
-#include "Hex.h";
+#include "Hex.h"
 #include <cstdlib>
+#include <iostream>
+
 using namespace std;
 
 int main()
@@ -7,6 +9,31 @@ int main()
 	HNumber a;
 	a.setNumber("1A");
 	a.print();
+
+	HNumber b;
+	b.setNumber("B3");
+	HNumber c;
+	c = a + b;
+	c.print();
+	cout << "a + b = " ; 
+	c.print(); cout << endl;
+	c = a - b;
+	cout << "a - b = ";
+	c.print(); cout << endl;
+	c = a * b;
+	cout << "a * b = ";
+	c.print(); cout << endl;
+	c = a / b;
+	cout << "a / b = ";
+	c.print(); cout << endl;
+	c = a % b;
+	c.print(); cout << endl;
+
+	cout << "a < b " << (a < b) << endl;
+	cout << "a > b " << (a > b) << endl;
+	cout << "a == b " << (a == b) << endl;
+	cout << "a <= b " << (a <= b) << endl;
+	cout << "a >= b " << (a >= b) << endl;
 
 	system("pause");
 	return 0;
