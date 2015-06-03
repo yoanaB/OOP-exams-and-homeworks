e0aa4fb6138e36b0a5ecc2bd7b4b306277edd30b
yoanaB <yoana.boyanova95@gmail.bg>
2015-06-03 20:14:23 +0300

    Update Hex.cpp
    Добавени декларации на операторите.

--- homework1/hw1_ioana_boqnova/hw1/hex.cpp
+++ homework1/hw1_ioana_boqnova/hw1/hex.cpp
@@ -43,18 +43,19 @@ void HNumber::setNumber(char* n)
 	strcpy_s(number, strlen(n) + 1, n);
 }
 
-char* HNumber::getNumber() const
+const char* HNumber::getNumber() const
 {
 	return number;
 }
 
-//void HNumber::read()
-//{
-//	cout << "Enter number in hexadecimal numeral system(the number of units is the first one!): ";
-//	do{
-//		cin.getline(number, 10);
-//	} while (*number < '0' || *number > '9' || *number < 'A' || *number > 'F');
-//}
+void HNumber::read()
+{
+	cout << "Enter number in hexadecimal numeral system(the number of units is the first one!): ";
+	do{
+		cin.getline(number, 10);
+		cin.ignore();
+	} while ((*number < '0' || *number > '9') || (*number < 'A' || *number > 'F'));
+}
 
 void HNumber::print() const
 {
@@ -63,7 +64,6 @@ void HNumber::print() const
 
 Hex::Hex()
 {
-	size = 100;
 	count = 0;
 }
 
@@ -74,9 +74,8 @@ Hex :: ~Hex()
 
 Hex::Hex(const Hex& other)
 {
-	this->size = other.size;
 	this->count = other.count;
-	for (int i = 0; i < other.size; i++)
+	for (int i = 0; i < 100; i++)
 	{
 		this->hex[i] = other.hex[i];
 	}
@@ -87,9 +86,8 @@ Hex& Hex :: operator= (const Hex& other)
 	if (this != &other)
 	{
 		delete[] hex;
-		this->size = other.size;
 		this->count = other.count;
-		for (int i = 0; i < other.size; i++)
+		for (int i = 0; i < 100; i++)
 		{
 			this->hex[i] = other.hex[i];
 		}
@@ -104,9 +102,9 @@ int Hex::getCount() const
 
 void Hex::addNumber(const HNumber& a)
 {
-	if (size == count)
+	if (maxSize == count)
 	{
-		size *= 2;
+		/*count *= 2;
 		HNumber* temp = new HNumber[count];
 		for (int i = 0; i < count; i++)
 		{
@@ -118,21 +116,159 @@ void Hex::addNumber(const HNumber& a)
 		{
 			hex[i] = temp[i];
 		}
-		delete[] temp;
+		delete[] temp;*/
+		cout << "The array is full!" << endl;
 	}
-	hex[size] = a;
+	else hex[count] = a;
 }
 
 void Hex::deleteNumber(char* n)
 {
-	for (int i = 0; i < size; i++)
+	bool number = false;
+	for (int i = 0; i < 100; i++)
 	{
 		if (strcmp(hex[i].getNumber(), n) == 0)
 		{
-			for (int j = i; j < size; j++)
+			number = true;
+			for (int j = i; j < 100; j++)
 			{
-				hex[i] = hex[i + 1];
+				hex[i + 1] = hex[i];
 			}
 		}
 	}
+	if (number)
+	{
+		HNumber* temp = new HNumber[count - 1];
+		for (int i = 0; i < count - 1; i++)
+		{
+			temp[i] = hex[i];
+		}
+		delete[] hex;
+		hex = temp;
+	}
+	else cout << "Cannot find the number!" << endl;
+}
+
+bool HNumber :: operator == (const HNumber &other)
+{
+	return (strcmp(this->number, other.number) == 0);
+}
+
+int HNumber::convertInInt(char* n)
+{
+	int dec = 0;
+	char current;
+	for (int i = 0; i < strlen(n); i++)
+	{
+		current = n[i];
+		if (n[i] >= 'A' && n[i] <= 'F')
+		{
+			dec += (current - 55) * pow(16, i);
+		}
+		else
+		{
+			dec += (current - 48) * pow(16, i);
+		}
+	}
+	return dec;
+}
+
+char* HNumber::convertInChar(int n)
+{
+	char* hex = new char[100];
+
+	int index = 0;
+	int remainder = 0;
+	while (n != 0)
+	{
+		remainder = n % 16;
+		if (remainder < 10)
+		{
+			hex[index] = char(remainder + 48);
+		}
+		else
+		{
+			hex[index] = char(remainder + 55);
+		}
+
+		index++;
+		n /= 16;
+	}
+
+	char* toReturn = new char[index + 1];
+	for (int i = 0; i < index; i++)
+	{
+		toReturn[i] = hex[i];
+	}
+	toReturn[index] = '\0';
+
+	delete[] hex;
+	return toReturn;
+}
+
+bool HNumber :: operator > ( HNumber &other)
+{
+		int sum1 = this->convertInInt(this->number);
+		int sum2 = other.convertInInt(other.number);
+		
+		return (sum1 > sum2);
+}
+
+bool HNumber :: operator < (HNumber &other)
+{
+		int sum1 = this->convertInInt(this->number);
+		int sum2 = other.convertInInt(other.number);
+
+		return (sum1 < sum2);
+}
+
+bool HNumber :: operator >= (HNumber &other)
+{
+	return !(this < &other);
+}
+
+bool HNumber :: operator <= (HNumber &other)
+{
+	return !(this > &other);
+}
+
+HNumber& HNumber :: operator + ( HNumber &other)
+{
+	int sum = this->convertInInt(this->number) + other.convertInInt(other.number);
+	HNumber* hexNumber = new HNumber(convertInChar(sum));
+	return *hexNumber;
+}
+
+HNumber& HNumber :: operator - ( HNumber &other)
+{
+	int sub = this->convertInInt(this->number) - other.convertInInt(other.number);
+	HNumber* hexNumber = new HNumber(convertInChar(sub));
+	return *hexNumber;
+}
+
+HNumber& HNumber :: operator *(HNumber & other)
+{
+	int pr = this->convertInInt(this->number) * other.convertInInt(other.number);
+	HNumber* hexNumber = new HNumber(convertInChar(pr));
+	return *hexNumber;
+}
+
+HNumber& HNumber :: operator /(HNumber & other)
+{
+	int del = this->convertInInt(this->number) / other.convertInInt(other.number);
+	HNumber* hexNumber = new HNumber(convertInChar(del));
+	return *hexNumber;
+}
+
+HNumber& HNumber :: operator %(HNumber & other)
+{
+	int delp = this->convertInInt(this->number) % other.convertInInt(other.number);
+	HNumber* hexNumber = new HNumber(convertInChar(delp));
+	return *hexNumber;
+}
+
+HNumber::HNumber(char* n)
+{
+	number = new char[strlen(n) + 1];
+	strcpy_s(number, strlen(n) + 1, n);
 }
