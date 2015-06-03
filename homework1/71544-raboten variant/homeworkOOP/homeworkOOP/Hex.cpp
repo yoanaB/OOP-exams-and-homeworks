7680b3a5e6563aa26f52d886cde5e8b492f058ee
elenastoyanovaa <elenastoyanovaaa@gmail.com>
2015-06-03 15:42:48 +0300

    Final

--- homework1/71544-raboten variant/homeworkOOP/homeworkOOP/Hex.cpp
+++ homework1/71544-raboten variant/homeworkOOP/homeworkOOP/Hex.cpp
@@ -7,9 +7,17 @@
 #define _CRT_SECURE_NO_WARNINGS
 using namespace std;
 
+char Hex::values[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+
+
 void Hex::print()const
 {
-	cout << "Hex:" << this->hex << endl;
+	cout << "Hex:";
+	for (int i = size-2; i>=0; i--)
+	{
+		cout << hex[i];
+	}
+	cout << endl;
 }
 Hex::Hex()
 {
@@ -19,34 +27,38 @@ Hex::Hex()
 }
 Hex::Hex(const Hex& other)
 {
-	this->hex = new char[other.size+1];
+	this->hex = new char[other.size];
+	this->size = other.size;
 	assert(hex != NULL);
-	for (int i = 0; i < other.size + 1; i++)
+	for (int i = 0; i < other.size -1; i++)
 	{
 		this->hex[i] = other.hex[i];
 	}
+	hex[size - 1] = '\0';
 }
 Hex::Hex(char* hex)
 {
-	this->size = strlen(hex);
+	this->size = strlen(hex)+1;
 	this->hex = new char[size + 1];
 	assert(hex != NULL);
-	for (int i = 0,j=size-1; i < size + 1; i++,j--)
+	for (int i = 0; i < size - 1; i++)
 	{
-		this->hex[i] = hex[j];
+		this->hex[i] = hex[size - 2 - i];
 	}
+	this->hex[size - 1] = '\0';
 }
 Hex& Hex::operator=(const Hex& other)
 {
 	if (this != &other)
 	{
 		delete[]hex;
-		this->hex = new char[other.size + 1];
+		this->hex = new char[other.size];
 		assert(hex != NULL);
-		for (int i = 0; i < other.size + 1; i++)
+		for (int i = 0; i < other.size -1; i++)
 		{
 			this->hex[i] = other.hex[i];
 		}
+		hex[size - 1] = '\0';
 	}
 	return *this;
 }
@@ -58,60 +70,64 @@ Hex::~Hex()
 Hex& Hex::operator+(const Hex& other)
 {
 	int value = fromHextoDec(*this) + fromHextoDec(other);
-	char*result=_itoa(value,hex, 16);
-	this->size = strlen(result);
-	this->hex = new char[strlen(result) + 1];
-	assert(this != NULL);
-	for (int i = 0,j=size-1; i < size; j--,i++)
+	char * result = new char[strlen(fromDectoHex(value))+1];
+	assert(result != NULL);
+	strcpy(result, fromDectoHex(value));
+	this->size = strlen(result) + 1;
+	this->hex = new char[size];
+	for (int i = 0; i < size-1; i++)
 	{
-		hex[i] = result[j];
+		this->hex[i] = result[i];
 	}
-	hex[size] = '\0';
+	hex[size-1] = '\0';
 	delete[] result;
 	return *this;
 }
 Hex& Hex::operator-(const Hex& other)
 {
 	int value = fromHextoDec(*this) - fromHextoDec(other);
-	char*result = _itoa(value, hex, 16);
-	this->size = strlen(result);
-	this->hex = new char[strlen(result) + 1];
-	assert(this != NULL);
-	for (int i = 0, j = size-1; i < size; j--, i++)
+	char * result = new char[strlen(fromDectoHex(value)) + 1];
+	assert(result != NULL);
+	strcpy(result, fromDectoHex(value));
+	this->size = strlen(result) + 1;
+	this->hex = new char[size];
+	for (int i = 0; i < size - 1; i++)
 	{
-		hex[i] = result[j];
+		this->hex[i] = result[i];
 	}
-	hex[size] = '\0';
+	hex[size - 1] = '\0';
 	delete[] result;
 	return *this;
 }
 Hex& Hex::operator*(const Hex& other)
 {
-	int value =( fromHextoDec(*this) ) * ( fromHextoDec(other));
-	char*result = _itoa(value, hex, 16);
-	this->size = strlen(result);
-	this->hex = new char[strlen(result) + 1];
-	assert(this != NULL);
-	for (int i = 0, j = size-1; i < size; j--, i++)
+	int value = fromHextoDec(*this) * fromHextoDec(other);
+	char * result = new char[strlen(fromDectoHex(value)) + 1];
+	assert(result != NULL);
+	strcpy(result, fromDectoHex(value));
+	this->size = strlen(result) + 1;
+	this->hex = new char[size];
+	for (int i = 0; i < size - 1; i++)
 	{
-		hex[i] = result[j];
+		this->hex[i] = result[i];
 	}
-	hex[size] = '\0';
+	hex[size - 1] = '\0';
 	delete[] result;
 	return *this;
 }
 Hex& Hex::operator/(const Hex& other)
 {
 	int value = fromHextoDec(*this) / fromHextoDec(other);
-	char*result = _itoa(value, hex, 16);
-	this->size = strlen(result);
-	this->hex = new char[strlen(result) + 1];
-	assert(this != NULL);
-	for (int i = 0, j = size; i < size; j--, i++)
+	char * result = new char[strlen(fromDectoHex(value)) + 1];
+	assert(result != NULL);
+	strcpy(result, fromDectoHex(value));
+	this->size = strlen(result) + 1;
+	this->hex = new char[size];
+	for (int i = 0; i < size - 1; i++)
 	{
-		hex[i] = result[j];
+		this->hex[i] = result[i];
 	}
-	hex[size] = '\0';
+	hex[size - 1] = '\0';
 	delete[] result;
 	return *this;
 }
@@ -142,18 +158,46 @@ bool Hex::operator!=(const Hex& other)
 }
 int Hex::fromHextoDec(const Hex& h)
 {
-	int result = 0;
-	for (int i = 0; i<h.size; i++) {
-		if (h.hex[i] >= 48 && h.hex[i] <= 57)
+	double dec = 0;
+	for (int i = 0; i < h.size-1; i++)
+	{
+		if ((h.hex[i] >= 'A' && h.hex[i] <= 'F') || (h.hex[i] >= '1' && h.hex[i] <= '9') || (h.hex[i] >= 'a' && h.hex[i] <= 'f'))
 		{
-			result += (h.hex[i] - 48)*pow(16, h.size - i - 1);
-		}
-		else if (h.hex[i] >= 65 && h.hex[i] <= 70) {
-			result += (h.hex[i] - 55)*pow(16, h.size - i - 1);
-		}
-		else if (h.hex[i] >= 97 && h.hex[i] <= 102) {
-			result += (h.hex[i] - 87)*pow(16, h.size - i - 1);
+			int value = 0;
+			switch (h.hex[i])
+			{
+			case 'A': case 'a': value = 10; break;
+			case 'B': case 'b': value = 11; break;
+			case 'C': case 'c': value = 12; break;
+			case 'D': case 'd': value = 13; break;
+			case 'E': case 'e': value = 14; break;
+			case 'F': case 'f': value = 15; break;
+			case '1': value = 1; break;
+			case '2': value = 2; break;
+			case '3': value = 3; break;
+			case '4': value = 4; break;
+			case '5': value = 5; break;
+			case '6': value = 6; break;
+			case '7': value = 7; break;
+			case '8': value = 8; break;
+			case '9': value = 9; break;
+			}
+			dec += value * (pow(16, double(i)));
 		}
 	}
-	return result;
+	return (int)dec;
+}
+char* Hex::fromDectoHex(int integer)
+{
+	char value[100];
+	int i = 0;
+	do
+	{
+		value[i] = values[integer % 16];
+		integer /= 16;
+		i++;
+	} while (integer > 0);
+
+	value[i] = '\0';
+	return value;
 }
